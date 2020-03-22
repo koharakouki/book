ed03daa091e33c002867304544840109aa75268c
koharakouki <ko.kohara02@gmail.com>
2020-03-23 00:40:07 +0900

    add strong-parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduction, :profile_image_id)
   end
 
   def logged_in_user
