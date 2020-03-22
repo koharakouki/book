e6170461248a20a7b19093f01f2cf956f3a0ef14
koharakouki <ko.kohara02@gmail.com>
2020-03-22 17:37:50 +0900

    add signup_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#home'
+  get 'home/about', to: 'static_pages#about'
+  get 'users/sign_up', to: 'users#new'
+  post 'users/sign_up', to: 'users#create'
+  resources :users
 end
