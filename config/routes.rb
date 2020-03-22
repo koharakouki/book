35ab9c7aa93d3f1a7b8b8c1a54abb6a2c3f659e4
koharakouki <ko.kohara02@gmail.com>
2020-03-22 19:09:21 +0900

    add login-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'static_pages#home'
   get 'home/about', to: 'static_pages#about'
   get 'users/sign_up', to: 'users#new'
   post 'users/sign_up', to: 'users#create'
+  get 'users/sign_in', to: 'sessions#new'
+  post 'users/sign_in', to: 'sessions#create'
+  delete 'users/sign_out', to: 'sessions#destroy'
   resources :users
 end
